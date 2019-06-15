2fdea72a7237a53e84321e57599703bcee92ed04
David Little <littledavid175@gmail.com>
2019-06-15 23:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ function placeOrder(cardNumber) {
   }else{ 
     var done=total()
     cart=[]
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${done}, which will be charged to the card ${cardNumber}.`
   }
 }

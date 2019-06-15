3271c3632c759fb96ce6016cccedf6cbb3bc0780
David Little <littledavid175@gmail.com>
2019-06-15 23:36:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,7 +58,8 @@ while(i<cart.length){
 function placeOrder(cardNumber) {
   if(cardNumber===undefined){
     return 'Sorry, we don\'t have a credit card on file for you.'
-  }else{
+  }else{ 
+    cart.
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
